0fc4d3d4d9cb2da1400b5bdeef665e644c542477
Herb Miller <herb@bobbingwide.com>
2017-01-29 17:51:23 +0000

    Issue #1 - Implement 'oik_a2z_display'

---
+++ includes/class-oik-a2z-display.php
@@ -0,0 +1,119 @@
+<?php // (C) Copyright Bobbing Wide 2017
+
+/**
+ * Class: OIK_a2z_display 
+ * 
+ * Displays letter taxonomy links for the current URL
+ * 
+ * @TODO - set current class for the currently selected taxonomy term
+ * 
+ *
+ */
+class OIK_a2z_display {
+	public $taxonomy;
+	public $post_type;
+
+	function __construct() {
+	}
+	
+	/** 
+	 * Displays links for the selected taxonomy
+	 *	
+ 	 */
+	function display( $taxonomy="letter" ) {
+		if ( '' === $taxonomy ) {
+			$taxonomy = "letter";
+		}
+		$this->taxonomy = $taxonomy;
+		$terms = $this->get_terms();
+		$this->display_term_list( $terms );
+		//$this->enqueue_styles();
+	}
+	
+	/**
+	 * Retrieves the terms for the taxonomy
+	 */
+	function get_terms() {
+		$args = array( "taxonomy" => $this->taxonomy
+								 , "hide_empty" => true
+								 );
+		$terms = get_terms( $args ); 
+		return( $terms );
+	}
+	
+	/**
+	 * Displays the terms for the taxonomy
+	 */
+	function display_term_list( $terms ) {
+		echo "<div class=\"a2z wrap archive-pagination pagination\">";
+		echo "<ul class=\"a2z {$this->taxonomy}\">";
+		foreach ( $terms as $term ) {
+			$this->display_term_item( $term );
+		}
+		echo "</ul>";
+		echo "</div>";
+	}
+	
+	/** 
+	 * Displays a term item.
+	
+			 [0] => WP_Term Object
+        (
+            [term_id] => 222
+            [name] => [
+            [slug] => 222
+            [term_group] => 0
+            [term_taxonomy_id] => 222
+            [taxonomy] => letter
+            [description] => 
+            [parent] => 0
+            [count] => 28
+            [filter] => raw
+            [meta] => Array
+                (
+                )
+								
+		 @TODO Mark the currently selected term with class 'current'							
+	 */
+	function display_term_item( $term ) {
+		if ( $term->count ) {
+			$term_link = get_term_link( $term, $this->taxonomy );
+			$term_string = $this->term_string( $term );
+			$link = retlink( "a2z_term", esc_url( $term_link ), $term_string, $term->name );
+		} else {
+			$link = retlink( "a2z_term empty", null, $term->name );
+		}
+		echo "<li>";
+		echo $link; 
+		echo "</li>";
+	}
+	
+	/**
+	 * Returns the term string
+	 * 
+	 * @param object $term
+	 * @return string 
+	 */
+	function term_string( $term ) {	
+		$term_string = $term->name;
+		$term_string .= retstag( "span", "count" );
+		$term_string .= $term->count;
+		$term_string .= retetag( "span" );
+		return( $term_string );
+	}
+	
+	/**
+	 * Enqueue the style sheet
+	 * 
+	 * @TODO Determine if this should be enqueued earlier to allow for quicker styling by the browser
+	 */
+	function enqueue_styles() {
+		$timestamp = null;
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+			$timestamp = filemtime( oik_path( "css/oik-a2z.css", "oik-a2z") );
+		}
+		wp_register_style( "oik-a2z", oik_url( "css/oik-a2z.css", "oik-a2z" ), array(), $timestamp );
+		wp_enqueue_style( "oik-a2z" );
+	}
+ 
+}
--- oik-a2z.php
+++ oik-a2z.php
@@ -4,13 +4,13 @@
 Plugin Name: oik-a2z
 Plugin URI: http://www.oik-plugins.com/oik-plugins/oik-a2z
 Description: Pagination by letter
-Version: 0.0.0
+Version: 0.0.1
 Author: bobbingwide
 Author URI: http://www.oik-plugins.com/author/bobbingwide
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-    Copyright 2016 Bobbing Wide (email : herb@bobbingwide.com )
+    Copyright 2016,2017 Bobbing Wide (email : herb@bobbingwide.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
@@ -30,15 +30,13 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 oik_a2z_loaded();
 
-//define( 'DEFAULT_TAXONOMY', "letter" );
-
 /**
  * Function to run when loaded
  */
 function oik_a2z_loaded() {
 	add_action( "init", "oik_a2z_init" );
 	add_action( "oik_fields_loaded", "oik_a2z_oik_fields_loaded" );
-	add_action( "oik_a2z_display", "oik_a2z_display" );
+	add_action( "oik_a2z_display", "oik_a2z_display", 10 );
 	add_action( "run_oik-a2z.php", "oik_a2z_run_oik_a2z" );
 	// Add further batch files if needed
 	//add_action( "run_filename", "oik_a2z_run_filename" );
@@ -64,18 +62,14 @@ function oik_a2z_oik_fields_loaded() {
 /**
  * Implement "oik_a2z_display" for oik-a2z
  *
- * Use this action to display links for all the categories in the selected taxonomy
- * applying to the current URL.
+ * Use this action to display links for all the categories in the selected taxonomy.
  * 
- * This is a bit like pagination. 
- * 
- * url&letter=A
- * url&letter=B 
- * url&letter=C
- * 
+ * @param string $taxonomy
  */
-function oik_a2z_display( $taxonomy, $url=null ) {
-	echo "Taxonomy display for $taxonomy";
+function oik_a2z_display( $taxonomy="letter" ) {
+	oik_require( "includes/class-oik-a2z-display.php", "oik-a2z" );
+	$oik_a2z_display = new OIK_a2z_display();
+	$oik_a2z_display->display( $taxonomy );
 }
 
 /**
@@ -84,12 +78,9 @@ function oik_a2z_display( $taxonomy, $url=null ) {
  * Letter | Meaning
  * ------ | -----------------------
  * A-Z    | Lower case or upper case letter including accented
- * 0-9    | The actual digit
  * #      | Representing the range of numeric digits 0-9
  * _      | When it really is an underscore
  * ?      | For punctuation and anything else
- * 
- * 
  *
  * @TODO Allow user selection of the letter terms - by applying a filter
  * 
@@ -103,7 +94,6 @@ function oik_a2z_get_letter_terms( $terms=null ) {
 	return( $terms );
 }
 
-
 /**
  * Run oik-a2z.php in batch
  *
@@ -112,6 +102,5 @@ function oik_a2z_get_letter_terms( $terms=null ) {
 function oik_a2z_run_oik_a2z() {
 	oik_require( "admin/oik-a2z-run.php", "oik-a2z" );
 	oik_a2z_lazy_run_oik_a2z();
-	
 }
 
